ab5910d69ee1ef5a11c07245d4e3a9e10b2f99c5
Boyan Marinov <boyan.marinov@yahoo.com>
2020-02-16 16:50:42 +0200

    Add user schema

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+const secret = require('../').secret;
+
+const UserSchema = new mongoose.Schema({
+    username: String,
+    email: String,
+    image: String,
+    salt: String,
+    hash: String
+}, {
+    timestamps: true
+});
+
+UserSchema.methods.setPassword = function (password) {
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+};
+
+UserSchema.methods.validPassword = function (password) {
+    const hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+    return this.hash === hash;
+};
+
+UserSchema.methods.generateJWT = function() {
+    const today = new Date();
+    const exp = new Date(today);
+    exp.setDate(today.getDate() + 60);
+
+    return jwt.sign({
+        id: this._id,
+        username: this.username,
+        exp: parseInt(exp.getTime() / 1000)
+    }, secret);
+};
+
+UserSchema.methods.toAuthJson = function() {
+    return {
+        username: this.username,
+        email: this.email,
+        token: this.generateJWT(),
+        image: this.image
+    };
+};
+
+mongoose.model('User', UserSchema);
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.listen(3000, function() {
     console.log('listening on 3000');
